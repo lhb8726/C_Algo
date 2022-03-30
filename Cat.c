1e6116e3c870f2c967a921a839719695441600f6
lhb8726 <lhb539@naver.com>
2022-03-30 01:25:36 +0000

    first commit

---
+++ Cat.c
@@ -0,0 +1,12 @@
+// 벡준 알고리즘
+// 문제 번호 : 10171
+
+#include <stdio.h>
+
+int main(void) {
+  printf("\\    /\\\n");
+  printf(" )  ( ')\n");
+  printf("(  /  )\n");
+  printf(" \\(__)|");
+  return 0;
+}
