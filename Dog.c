ae2b01b99af87a49e15f94b817508cb3dfd3191e
lhb8726 <lhb5329@naver.com>
2022-03-31 01:09:26 +0000

    first commit

---
+++ Dog.c
@@ -0,0 +1,13 @@
+// 백준 알고리즘
+// 문제 번호 : 10172
+
+#include <stdio.h>
+
+int main(void) {
+  printf("|\\_/|\n");
+  printf("|q p|   /}\n");
+  printf("( 0 )\"\"\"\\\n");
+  printf("|\"^\"`    |\n");
+  printf("||_/=\\\\__|");
+  return 0;
+}
