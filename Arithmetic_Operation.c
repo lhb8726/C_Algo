0a40a17e826b74636efdb5eb15dae528c6e36789
lhb8726 <lhb5329@naver.com>
2022-04-07 01:33:13 +0000

    first commit

---
+++ Arithmetic_Operation.c
@@ -0,0 +1,15 @@
+// 백준 알고리즘
+// 문제 번호 : 10869
+
+#include <stdio.h>
+
+int main(void) {
+  int a, b;
+  scanf("%d %d", &a, &b);
+  printf("%d\n", (a + b));
+  printf("%d\n", (a - b));
+  printf("%d\n", (a * b));
+  printf("%d\n", (a / b));
+  printf("%d\n", (a % b));
+  return 0;
+}
