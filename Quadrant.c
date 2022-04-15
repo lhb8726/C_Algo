7170cee7a29cca67774f0d6c858c57649270d4de
lhb8726 <lhb5329@naver.com>
2022-04-15 03:36:09 +0000

    first commit

---
+++ Quadrant.c
@@ -0,0 +1,24 @@
+// 백준 알고리즘
+// 문제 번호 : 14681
+
+#include <stdio.h>
+
+int main(void) {
+  int a, b;
+  scanf("%d", &a);
+  scanf("%d", &b);
+  if(a > 0) {
+    if(b > 0) {
+      printf("1");
+    }else {
+      printf("4");
+    }
+  }else {
+    if(b > 0) {
+      printf("2");
+    }else {
+      printf("3");
+    }
+  }
+  return 0;
+}
